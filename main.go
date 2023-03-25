492f9d1d05500a25ca8bcf4160d547253bf87ecb
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-24 23:05:44 -0600

    showing the json

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
 
+	"github.com/anibal-alpizar/go-cli-crud/tasks"
+)
 
 func main() {
-	
+	// read or create the tasks.json file
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var taskss []tasks.Task
+
+	// get file info
+	info, err := file.Stat()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// check the size
+	if info.Size() != 0 {
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(bytes, &taskss)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		taskss = []tasks.Task{}
+	}
+
+	fmt.Println(taskss)
 }
